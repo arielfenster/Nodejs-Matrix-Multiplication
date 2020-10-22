051ea187500131e09a5ba85c15507fd63dbbb203
ariel_fenster <arielfenster1610@gmail.com>
2020-10-22 22:54:59 +0300

    created endpoint for user to upload files. used with multer and streamifier

--- src/routes/matrix.js
+++ src/routes/matrix.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { generateMatrix, parseMatrix, saveMatrixToFile } from '../utils/matrix';
 import path from 'path';
+import fs from 'fs';
+import multer from 'multer';
+import streamifier from 'streamifier';
+import { generateMatrix, saveMatrixToFile } from '../utils/matrix';
+import { MATRIX_CONSTS, UPLOAD_CONSTS } from '../utils/constants';
 
+// Create a multer middleware for uploading files (create an empty one so we can create streams instead of instantaneous upload)
+const upload = multer();
 
 const router = express.Router();
 
@@ -14,26 +20,39 @@ router.post('/generate', (req, res) => {
   
   const matrix = generateMatrix({ height, width });
   
-  saveMatrixToFile('random.txt', matrix);
+  saveMatrixToFile(MATRIX_CONSTS.RANDOM_MATRIX_FILENAME, matrix);
 
-  res.status(201).sendFile(path.join(__dirname, '../data/random.txt'), (err) => {
+  res.status(201).sendFile(path.join(__dirname, `../data/${MATRIX_CONSTS.RANDOM_MATRIX_FILENAME}`), (err) => {
     if (err) { res.status(500).send(err) }
   });
 });
 
 
 /**
- * @description 
+ * @description Upload user's matrices
+ */
+router.post('/', upload.fields([
+  { name: UPLOAD_CONSTS.MATRIX_A, maxCount: 1 },
+  { name: UPLOAD_CONSTS.MATRIX_B, maxCount: 1 }]) ,(req, res) => {
+    const { files } = req;
+
+    for (const fileKey in files) {
+      const file = files[fileKey][0];
+      const { buffer } = file;
+      
+      const writeStream = fs.createWriteStream(path.join(__dirname, `../data/${fileKey}.txt`));
+      streamifier.createReadStream(buffer).pipe(writeStream);
+    }
+
+    res.status(200).send('Uploaded the files');
+});
+
+
+/**
+ * @description Calculate the product of the matrices
  */
 router.get('/', (req, res) => {
-  // const stream = fs.createReadStream(__dirname + '/data.txt', { encoding: 'utf-8'});
-  // stream.pipe(res);
-  // stream.on('data', (chunk) => {
-  //   console.log('received');
-  // });
-  // stream.on('end', () => {
-  //   res.status(200).send();
-  // })
+
 });
 
 export default router;
